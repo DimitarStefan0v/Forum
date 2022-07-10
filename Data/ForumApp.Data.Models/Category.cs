84a5c48c56f98ed2a2a3743a0d7ab0bdaab70887
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-07-10 23:06:42 +0300

    Add Post entity

--- Data/ForumApp.Data.Models/Category.cs
+++ Data/ForumApp.Data.Models/Category.cs
@@ -1,14 +1,22 @@
 ﻿namespace ForumApp.Data.Models
 {
+    using System.Collections.Generic;
+
     using ForumApp.Data.Common.Models;
 
     public class Category : BaseDeletableModel<int>
     {
+        public Category()
+        {
+            this.Posts = new HashSet<Post>();
+        }
+
         public string Name { get; set; }
 
         public string Title { get; set; }
 
         public string Description { get; set; }
 
+        public ICollection<Post> Posts { get; set; }
     }
 }
---
+++ Data/ForumApp.Data.Models/Post.cs
@@ -0,0 +1,19 @@
+﻿namespace ForumApp.Data.Models
+{
+    using ForumApp.Data.Common.Models;
+
+    public class Post : BaseDeletableModel<int>
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string AddedByUserId { get; set; }
+
+        public ApplicationUser AddedByUser { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
