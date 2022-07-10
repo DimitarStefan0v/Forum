d9292c8c0e8a0a6f1b22ad270a84e2861668e6e8
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-07-10 22:52:35 +0300

    Add base Category entity

---
+++ Data/ForumApp.Data.Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace ForumApp.Data.Models
+{
+    using ForumApp.Data.Common.Models;
+
+    public class Category : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
