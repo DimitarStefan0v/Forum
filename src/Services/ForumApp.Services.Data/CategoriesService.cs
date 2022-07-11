be67526ce4f1709072ad951f418344a4a4c6c1e8
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-07-11 23:07:21 +0300

    Add CategoriesService

---
+++ src/Services/ForumApp.Services.Data/CategoriesService.cs
@@ -0,0 +1,36 @@
+﻿namespace ForumApp.Services.Data
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using ForumApp.Data.Common.Repositories;
+    using ForumApp.Data.Models;
+    using ForumApp.Services.Mapping;
+
+    public class CategoriesService : ICategoriesService
+    {
+        private readonly IDeletableEntityRepository<Category> categoriesRepository;
+
+        public CategoriesService(IDeletableEntityRepository<Category> categoriesRepository)
+        {
+            this.categoriesRepository = categoriesRepository;
+        }
+
+        public IEnumerable<T> GetAll<T>(int? count = 10)
+        {
+            var query = this.categoriesRepository
+                .All()
+                .OrderBy(x => x.Title);
+
+            if (count.HasValue)
+            {
+                query
+                    .Take(count.Value);
+            }
+
+            return query
+                .To<T>()
+                .ToList();
+        }
+    }
+}
