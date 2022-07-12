3a5fab9c14fc1fc3fc0c64473ab6df7ecfcfc68a
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-07-12 11:02:18 +0300

    Add SingleCategoryViewModel

---
+++ src/Web/ForumApp.Web.ViewModels/Categories/SingleCategoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace ForumApp.Web.ViewModels.Categories
+{
+    using System.Collections.Generic;
+
+    using ForumApp.Data.Models;
+    using ForumApp.Services.Mapping;
+
+    public class SingleCategoryViewModel : IMapFrom<Category>
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<SubCategoriesInCategoryViewModel> SubCategories { get; set; }
+    }
+}
