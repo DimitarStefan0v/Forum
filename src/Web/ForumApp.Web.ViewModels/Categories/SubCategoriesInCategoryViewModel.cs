873bd3191c6d2c831ca883d9c3d6951437715599
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-07-12 11:02:31 +0300

    Add SubCategoriesInCategoryViewModel

---
+++ src/Web/ForumApp.Web.ViewModels/Categories/SubCategoriesInCategoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ForumApp.Web.ViewModels.Categories
+{
+    using System.Collections.Generic;
+
+    using ForumApp.Data.Models;
+    using ForumApp.Services.Mapping;
+
+    public class SubCategoriesInCategoryViewModel : IMapFrom<SubCategory>
+    {
+        public string Title { get; set; }
+
+        public string Name { get; set; }
+        
+
+    }
+}
