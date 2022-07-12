ede5b718fb7fc8b9f1954eabe7e2f77983bddc1f
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-07-12 10:45:07 +0300

    Add new Custom Route for main categories

--- src/Web/ForumApp.Web/Program.cs
+++ src/Web/ForumApp.Web/Program.cs
@@ -101,6 +101,7 @@
             app.UseAuthorization();
 
             app.MapControllerRoute("areaRoute", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+            app.MapControllerRoute("forumMainCategory", "parent-category/{name:minlength(2)}", new { controller = "Categories", action = "ByName" });
             app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             app.MapRazorPages();
         }
