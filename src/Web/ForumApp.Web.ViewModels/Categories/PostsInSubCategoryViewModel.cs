c7a8327dd47ca0c0783ccf363307c170b98921ca
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-07-12 11:08:07 +0300

    Add PostInSubCategoryViewModel

---
+++ src/Web/ForumApp.Web.ViewModels/Categories/PostsInSubCategoryViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace ForumApp.Web.ViewModels.Categories
+{
+    using ForumApp.Data.Models;
+    using ForumApp.Services.Mapping;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class PostsInSubCategoryViewModel : IMapFrom<Post>
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string UserUserName { get; set; }
+
+        public ICollection<CommentsInPostViewModel> Comments { get; set; }
+    }
+}
--- src/Web/ForumApp.Web.ViewModels/Categories/SubCategoriesInCategoryViewModel.cs
+++ src/Web/ForumApp.Web.ViewModels/Categories/SubCategoriesInCategoryViewModel.cs
@@ -1,7 +1,5 @@
 ﻿namespace ForumApp.Web.ViewModels.Categories
 {
-    using System.Collections.Generic;
-
     using ForumApp.Data.Models;
     using ForumApp.Services.Mapping;
 
@@ -10,7 +8,14 @@
         public string Title { get; set; }
 
         public string Name { get; set; }
-        
 
+        public string Description { get; set; }
+
+        public string UserUserName { get; set; }
+
+        public string Url => $"/f/{this.Name.Replace(' ', '-').ToLower()}";
+
+        // TODO: add image
+        public ICollection<PostsInSubCategoryViewModel> Posts { get; set; }
     }
 }
