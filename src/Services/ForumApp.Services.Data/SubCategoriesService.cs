5031a1a9202e2366376c43eace26850f137cb9c6
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-07-14 09:34:17 +0300

    Add its implementation

---
+++ src/Services/ForumApp.Services.Data/SubCategoriesService.cs
@@ -0,0 +1,32 @@
+﻿namespace ForumApp.Services.Data
+{
+    using ForumApp.Data.Common.Repositories;
+    using ForumApp.Data.Models;
+    using ForumApp.Services.Mapping;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SubCategoriesService : ISubCategoriesService
+    {
+        private readonly IDeletableEntityRepository<SubCategory> subCategoriesRepository;
+
+        public SubCategoriesService(IDeletableEntityRepository<SubCategory> subCategoriesRepository)
+        {
+            this.subCategoriesRepository = subCategoriesRepository;
+        }
+
+        public T GetSubCategoryByName<T>(string name)
+        {
+            var searchName = name.Replace('-', ' ').Trim();
+
+            return this.subCategoriesRepository
+                .AllAsNoTracking()
+                .Where(x => x.Name == searchName)
+                .To<T>()
+                .FirstOrDefault();
+        }
+    }
+}
