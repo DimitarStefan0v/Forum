c0be8c5e9736b1f2e2236e394dcf412f1a503388
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-07-15 18:11:46 +0300

    Add PostCreateInputModel input model

---
+++ src/Web/ForumApp.Web.ViewModels/Posts/PostCreateInputModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ForumApp.Web.ViewModels.Posts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class PostCreateInputModel
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int SubCategoryId { get; set; }
+    }
+}
