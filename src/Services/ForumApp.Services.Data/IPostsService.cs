94b6dae0ff4f2c37cd582c038bede906858cccfc
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-07-15 18:42:11 +0300

    Add IPostsService interface

---
+++ src/Services/ForumApp.Services.Data/IPostsService.cs
@@ -0,0 +1,11 @@
+﻿namespace ForumApp.Services.Data
+{
+    using System.Threading.Tasks;
+
+    using ForumApp.Web.ViewModels.Posts;
+
+    public interface IPostsService
+    {
+        Task CreateAsync(PostCreateInputModel input, string userId);
+    }
+}
